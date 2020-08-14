035db9bbd0996cac84a66a7951234166dbb247f7
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-08-14 19:06:20 +0300

    Initial translation

--- Tests/DataStructureTests/XCTestManifests.swift
+++ Tests/DataStructureTests/XCTestManifests.swift
@@ -5,6 +5,7 @@ public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(CacheLinkedListTest.allTests),
         testCase(LRUCacheTest.allTests),
+        testCase(CounterHashMapTest.allTests),
     ]
 }
 #endif
