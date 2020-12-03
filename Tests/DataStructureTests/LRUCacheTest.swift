4c8ef310fec0fcb6f0ac31a5ace5999089883e1e
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-12-03 09:31:17 +0300

    Added cache test.

--- Tests/DataStructureTests/LRUCacheTest.swift
+++ Tests/DataStructureTests/LRUCacheTest.swift
@@ -44,11 +44,29 @@ final class LRUCacheTest: XCTestCase {
         }
         XCTAssertEqual(0.632, count / 1000000.0, accuracy: 0.001)
     }
+    
+    func test5(){
+        let size : Int = 100000
+        let cache : LRUCache = LRUCache<Int, Int>(cacheSize: size)
+        for i in 0...size{
+            cache.add(key: i, data: i)
+        }
+        for i in 0...100{
+            let time1 = Int64(Date().timeIntervalSince1970.magnitude * 1000)
+            for j in 0...size{
+                let data = Int.random(in: size..<2 * size)
+                cache.add(key: data, data: data)
+            }
+            let time2 = Int64(Date().timeIntervalSince1970.magnitude * 1000)
+            print(time2 - time1)
+        }
+    }
 
     static var allTests = [
         ("testExample1", test1),
         ("testExample2", test2),
         ("testExample3", test3),
         ("testExample4", test4),
+        ("testExample5", test5),
     ]
 }
